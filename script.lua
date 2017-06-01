0de734ed5f23148c2507f58766875195b2654794
Mathieu-Seurin <matseurin@cegetel.com>
2017-06-01 09:34:33 +0200

    Big commit : summing gradient, decorrelationg batches, preloading data etc..

--- script.lua
+++ script.lua
@@ -5,8 +5,16 @@ require 'torch'
 require 'xlua'
 require 'math'
 require 'string'
-require 'cunn'
 require 'nngraph'
+
+-- THIS IS WHERE ALL THE CONSTANTS SHOULD COME FROM
+-- See const.lua file for more details
+require 'const'
+-- try to avoid global variable as much as possible
+
+if USE_CUDA then
+   require 'cunn'
+end
 
 require 'MSDC'
 require 'functions'
@@ -14,12 +22,8 @@ require 'printing'
 require "Get_Images_Set"
 require 'optim_priors'
 require 'definition_priors'
--- THIS IS WHERE ALL THE CONSTANTS SHOULD COME FROM
--- See const.lua file for more details
-require 'const'
--- try to avoid global variable as much as possible
 
-function Rico_Training(Models,data1,data2)
+function Rico_Training(Models)
 
    local rep_criterion=get_Rep_criterion()
    local prop_criterion=get_Prop_criterion()
@@ -42,38 +46,38 @@ function Rico_Training(Models,data1,data2)
 
       --===========
       local mode='Temp' --Same for continuous or not
-      local batch=getRandomBatchFromSeparateList(data1,data2,BATCH_SIZE,mode)
+      local batch=getRandomBatchFromSeparateList(BATCH_SIZE,mode)
       LOSS_TEMP,grad=doStuff_temp(Models,temp_criterion, batch,COEF_TEMP)
 
       if USE_CONTINUOUS then
          --==========
          mode='Prop'
-         batch, action1, action2 = getRandomBatchFromSeparateListContinuous(data1,data2,BATCH_SIZE,mode)
+         batch, action1, action2 = getRandomBatchFromSeparateListContinuous(BATCH_SIZE,mode)
          LOSS_PROP,gradProp=doStuff_Prop_continuous(Models,prop_criterion,batch,COEF_PROP, action1, action2)
 
          --==========
          mode='Caus'
-         batch, action1, action2 = getRandomBatchFromSeparateListContinuous(data1,data2,BATCH_SIZE,mode)
+         batch, action1, action2 = getRandomBatchFromSeparateListContinuous(BATCH_SIZE,mode)
          LOSS_CAUS,gradCaus=doStuff_Caus_continuous(Models,caus_criterion,batch,COEF_CAUS, action1, action2)
 
          --==========
          mode='Rep'
-         batch, action1, action2 = getRandomBatchFromSeparateListContinuous(data1,data2,BATCH_SIZE,mode)
+         batch, action1, action2 = getRandomBatchFromSeparateListContinuous(BATCH_SIZE,mode)
          LOSS_REP,gradRep=doStuff_Rep_continuous(Models,rep_criterion,batch,COEF_REP, action1, action2)
       else
          --==========
          mode='Prop'
-         batch = getRandomBatchFromSeparateList(data1,data2,BATCH_SIZE,mode)
+         batch = getRandomBatchFromSeparateList(BATCH_SIZE,mode)
          LOSS_PROP,gradProp=doStuff_Prop(Models,prop_criterion,batch,COEF_PROP)
 
          --==========
          mode='Caus'
-         batch = getRandomBatchFromSeparateList(data1,data2,BATCH_SIZE,mode)
+         batch = getRandomBatchFromSeparateList(BATCH_SIZE,mode)
          LOSS_CAUS,gradCaus=doStuff_Caus(Models,caus_criterion,batch,COEF_CAUS)
 
          --==========
          mode='Rep'
-         batch=getRandomBatchFromSeparateList(data1,data2,BATCH_SIZE,mode)
+         batch=getRandomBatchFromSeparateList(BATCH_SIZE,mode)
          LOSS_REP,gradRep=doStuff_Rep(Models,rep_criterion,batch,COEF_REP)
       end
 
@@ -82,7 +86,7 @@ function Rico_Training(Models,data1,data2)
 
    --sgdState = sgdState or { learningRate = LR, momentum = mom,learningRateDecay = 5e-7,weightDecay=coefL2 }
    --parameters, loss=optim.sgd(feval, parameters, sgdState)
-   optimState={learningRate=LR}
+   optimState={learningRate=LR, learningRateDecay=LR_DECAY}
 
    if SGD_METHOD == 'adagrad' then
       parameters,loss=optim.adagrad(feval,parameters,optimState)
@@ -99,7 +103,7 @@ function train_Epoch(Models,Prior_Used,LOG_FOLDER,LR, USE_CONTINUOUS)
    local NB_BATCHES= math.ceil(NB_SEQUENCES*90/BATCH_SIZE/(4+4+2+2))
    --90 is the average number of images per sequences, div by 12 because the network sees 12 images per iteration
    -- (4*2 for rep and prop, 2*2 for temp and caus)
-
+   
    print(NB_SEQUENCES..' : sequences. '..NB_BATCHES..' batches')
 
    for epoch=1, NB_EPOCHS do
@@ -109,23 +113,8 @@ function train_Epoch(Models,Prior_Used,LOG_FOLDER,LR, USE_CONTINUOUS)
       xlua.progress(0, NB_BATCHES)
 
       for numBatch=1, NB_BATCHES do
-         indice1=torch.random(1,NB_SEQUENCES-1)
-         indice2=torch.random(1,NB_SEQUENCES-1)
 
-         ------------- only one list used----------
-         -- print([[====================================================
-         -- WARNING TESTING PRIOR, THIS IS NOT RANDOM AT ALL
-         -- ====================================================]])
-         -- local indice1=8
-         -- local indice2=3
-
-         local data1 = load_seq_by_id(indice1)
-         local data2 = load_seq_by_id(indice2)
-
-         assert(data1, "Something went wrong while loading data1")
-         assert(data2, "Something went wrong while loading data2")
-
-         Loss,Grad=Rico_Training(Models,data1,data2,LR,BATCH_SIZE, USE_CONTINUOUS)
+         Loss,Grad=Rico_Training(Models)
 
          total_temp_loss = total_temp_loss + LOSS_TEMP --Ugly, this variable is located in Rico_Training in eval
          total_caus_loss = total_caus_loss + LOSS_CAUS --Ugly, this variable is located in Rico_Training in eval
@@ -171,10 +160,17 @@ Tests_Todo={
    --]]
 }
 
-
-
 local list_folders_images, list_txt_action,list_txt_button, list_txt_state=Get_HeadCamera_View_Files(DATA_FOLDER)
 NB_SEQUENCES= #list_folders_images
+
+if CAN_HOLD_ALL_SEQ_IN_RAM then
+   print("Preloading all sequences in memory, that way, to accelerate batch selection")
+   ALL_SEQ = {} -- Preload all the sequences instead of loading specific sequences during batch selection
+   for id=1,NB_SEQUENCES do
+      ALL_SEQ[#ALL_SEQ+1] = load_seq_by_id(id)
+   end
+end
+
 
 for nb_test=1, #Tests_Todo do
 
