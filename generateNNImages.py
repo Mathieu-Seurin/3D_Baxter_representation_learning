30e6190b640c00852ec93c1481113adf26838a8d
Mathieu-Seurin <matseurin@cegetel.com>
2017-06-13 17:19:01 +0200

    Adding NN-qualitative-criterion

--- generateNNImages.py
+++ generateNNImages.py
@@ -7,9 +7,16 @@ import shutil
 import random
 import sys
 
+import subprocess
+
 nbr_images = -1
+
+subprocess.call(['th','create_plotStates_file_for_all_seq.lua'])
+
+ALL_STATE_FILE = 'allStates.txt'
 LEARNED_REPRESENTATIONS_FILE = "saveImagesAndRepr.txt"
 LAST_MODEL_FILE = 'lastModel.txt'
+
 
 if len(sys.argv) <= 1:
     sys.exit("Give number of neighbors to produce, followed by number of input images (and model dir if you don't want to use the last model created)")
@@ -26,24 +33,39 @@ else:
     lastModelFile = open(LAST_MODEL_FILE)
     path_to_model = lastModelFile.readline()[:-1]
 
-data_file=path_to_model+"/"+LEARNED_REPRESENTATIONS_FILE
+file_representation_string=path_to_model+"/"+LEARNED_REPRESENTATIONS_FILE
+
+
+#Parsing representation file
+#===================
 
 #reading data
-file  = open(data_file, "r")
+file_representation  = open(file_representation_string, "r")
 
+#parsing
 images=[]
-states=[]
-for line in file:
+representations=[]
+for line in file_representation:
     if line[0]!='#':
         words = line.split()
         images.append(words[0])
-        states.append(words[1:])
+        representations.append(words[1:])
 
-dim_state= len(states[0])
+
+#Parsing true state file
+#===================
+true_states = {}
+file_state = open(ALL_STATE_FILE, "r")
+
+for line in file_state:
+    if line[0]!='#':
+        words = line.split()
+        true_states[words[0]] = np.array(map(float,words[1:]))
+
 
 #Compute nearest neighbors
-nbrs = NearestNeighbors(n_neighbors=(nbr_neighbors+1), algorithm='ball_tree').fit(states)
-distances, indexes = nbrs.kneighbors(states)
+nbrs = NearestNeighbors(n_neighbors=(nbr_neighbors+1), algorithm='ball_tree').fit(representations)
+distances, indexes = nbrs.kneighbors(representations)
 
 #Generate mosaics
 path_to_neighbour = path_to_model + '/NearestNeighbors/'
@@ -54,10 +76,20 @@ if not os.path.exists(path_to_neighbour):
 	os.mkdir(path_to_neighbour)
 
 if nbr_images == -1:
-	data= zip(images,indexes,distances,states)
+	data= zip(images,indexes,distances,representations)
 else:
-	data= random.sample(zip(images,indexes,distances,states),nbr_images)
+	data= random.sample(zip(images,indexes,distances,representations),nbr_images)
 
+
+# For each random selected images (or all images in nbr_images==-1), you take
+# the k-nearest neighbour in the REPRESENTATION SPACE (the first argv parameter)
+
+#As a quantitative measure, for the k nearest neighbour
+#you compute the distance between the state of the original image and 
+#the images retrieved using knn on representation space
+
+total_error = 0 # to assess the quality of repr
+nb_tot_img = 0
 
 for img_name,id,dist,state in data:
 	base_name= os.path.splitext(os.path.basename(img_name))[0]
@@ -72,20 +104,28 @@ for img_name,id,dist,state in data:
 	state_str='[' + ",".join(['{:.3f}'.format(float(x)) for x in state]) + "]"
 	a.set_title(seq_name + "/" + base_name + ": \n" + state_str)
 
+        original_coord = true_states[img_name]
+
 	for i in range(0,nbr_neighbors):
 		a=fig.add_subplot(1,nbr_neighbors+1,i+2)
 		img_name=images[id[i+1]]
 		img = mpimg.imread(img_name)
 		imgplot = plt.imshow(img)
 
+
 		base_name_n= os.path.splitext(os.path.basename(img_name))[0]
 		seq_name_n= img_name.split("/")[1]
 
 		dist_str = ' d=' + '{:.4f}'.format(dist[i+1])
 
-		state_str='[' + ",".join(['{:.3f}'.format(float(x)) for x in states[id[i+1]]]) + "]"
+		state_str='[' + ",".join(['{:.3f}'.format(float(x)) for x in representations[id[i+1]]]) + "]"
 		a.set_title(seq_name_n + "/" + base_name_n + ": \n" + state_str +dist_str)
 		a.axis('off')
+
+                neighbour_coord = true_states[img_name]
+                total_error += np.linalg.norm(neighbour_coord-original_coord)
+                nb_tot_img += 1
+
 
 	plt.tight_layout()
 	output_file = path_to_neighbour + seq_name + "_" + base_name
@@ -93,5 +133,11 @@ for img_name,id,dist,state in data:
 	plt.savefig(output_file,bbox_inches='tight')
 	plt.close() # efficiency: avoids keeping all images into RAM
 
+mean_error = total_error/nb_tot_img
+print "MEAN ERROR : ", str(mean_error)[:5]
 
-print('Saved nearest neighbor images to '+output_file)
+f = open(path_to_model+'/scoreNN.txt','w')
+f.write(str(mean_error)[:5])
+f.close()
+
+
