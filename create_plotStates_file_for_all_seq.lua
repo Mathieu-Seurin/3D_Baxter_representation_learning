206ab5eee976c6afdcd481f33ca2b9bce2eafe6b
NataliaDiaz <diaz.rodriguez@gmail.com>
2017-06-23 15:23:50 +0200

    hyperparam gridsearch for continuous actions coherent now, also default dataset now is always STATIC_BUTTON_SIMPLEST

--- create_plotStates_file_for_all_seq.lua
+++ create_plotStates_file_for_all_seq.lua
@@ -95,7 +95,7 @@ end
 local cmd = torch.CmdLine()
 cmd:option('-use_cuda', false, 'true to use GPU, false (default) for CPU only mode')
 cmd:option('-use_continuous', false, 'true to use a continuous action space, false (default) for discrete one (0.5 range actions)')
-cmd:option('-data_folder', MOBILE_ROBOT, 'Possible Datasets to use: staticButtonSimplest, mobileRobot, staticButtonSimplest, simpleData3D, pushingButton3DAugmented, babbling')
+cmd:option('-data_folder', STATIC_BUTTON_SIMPLEST, 'Possible Datasets to use: staticButtonSimplest, mobileRobot, staticButtonSimplest, simpleData3D, pushingButton3DAugmented, babbling')
 cmd:option('-mcd', 0.4, 'Max. cosine distance allowed among actions for priors loss function evaluation (MAX_COS_DIST_AMONG_ACTIONS_THRESHOLD)')
 cmd:option('-sigma', 0.6, "Sigma: denominator in continuous actions' extra factor (CONTINUOUS_ACTION_SIGMA)")
 
