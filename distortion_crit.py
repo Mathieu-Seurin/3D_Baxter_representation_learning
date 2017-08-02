df25a64fee4079ffafe813a6e58d0235ddb74fc1
NataliaDiaz <diaz.rodriguez@gmail.com>
2017-08-02 13:38:55 +0200

    fixing distorsion updated param

--- distortion_crit.py
+++ distortion_crit.py
@@ -25,7 +25,7 @@ data_folder = get_data_folder_from_model_name(path_to_model)
 file_representation_string=path_to_model+"/"+LEARNED_REPRESENTATIONS_FILE
 images, representations = parse_repr_file(file_representation_string)
 
-true_states = parse_true_state_file()
+true_states = parse_true_state_file(data_folder)
 
 
 # convert learned states to numpy
@@ -81,5 +81,3 @@ print far_str
 score_file.write(far_str+'\n')
 
 score_file.close()
-
-
